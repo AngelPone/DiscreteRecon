6f801d78c2b839d7096861e893b9008e6ee1b362
Bohan Zhang <zhangbohan@buaa.edu.cn>
2022-07-04 22:04:56 +0800

    add point metrics

--- R/evaluation.R
+++ R/evaluation.R
@@ -7,7 +7,29 @@
 brier_score <- function(probf, y) {
   time_window <- dim(probf)[1]
   stopifnot(is(y, "dhts"))
-  sum((probf - cons_realDummy(y))^2)/time_window
+  if (is(probf, "jdist-bu") | is(probf, "jdist-rec") | is(probf, "jdist-td")){
+    bs <- sum((probf - cons_realDummy(y))^2)/time_window
+  } else if (is(probf, "jdist-ind")){
+    flags <- getCoherentFlag(y$domain$incoherent_domain, y$domain$coherent_domain)
+    cps <- probf[,flags$coherent]
+    ips <- probf[,flags$incoherent]
+    cps <- sum((cps - cons_realDummy(y))^2)/time_window
+    return(c(coherency=cps, incoherency=sum(ips^2)/time_window))
+  }
+}
+
+#' utility functions
+getCoherentFlag <- function(idomain, cdomain){
+  output <- list()
+  coherent <- NULL
+  for (i in 1:dim(idomain)[1]){
+    for (j in 1:dim(cdomain)[1]){
+      if (all(idomain[i,] == cdomain[j,])){
+        coherent <- c(coherent, i)
+      }
+    }
+  }
+  list(coherent=coherent, incoherent=setdiff(1:dim(idomain)[1], coherent))
 }
 
 
@@ -22,9 +44,25 @@ point_forecast <- function(dist, domain){
   fms <- matrix(0, nrow = dim(dist)[1], ncol = dim(domain)[2])
   for (i in 1:dim(domain)[2]){
     fi <- Joint2Marginal(dist, domain, i)
-    fm <- apply(fi, 1, function(x){sum((0:(dim(fi)[2]-1)) * x)})
+    ds <- as.numeric(colnames(fi))
+    fm <- apply(fi, 1, function(x){sum(ds * x)})
     fms[,i] <- fm
   }
+  colnames(fms) <- colnames(domain)
   return(fms)
+}
+
+#' Point metrics of each series
+#' @param dist
+#' @param y dhts
+#' @param f function to calculate metric
+#' @return vector containing metric of each series
+#' @export
+point_metric <- function(dist, y, f){
+  pointf <- point_forecast(dist, y$domain$coherent_domain)
+  all_y <- aggdhts(y)
+  sapply(1:dim(all_y)[2], function(x){
+    f(pointf[,x], all_y[,x])
+  })
 }
 
--- tests/testthat/test-evaluation.R
+++ tests/testthat/test-evaluation.R
@@ -1,5 +1,31 @@
-test.brier_score <- function(){
-  probf <- matrix(c(0.1, 0.2, 0.3, 0.9, 0.8, 0.7), 3)
-  real <- matrix(c(1, 0, 0, 1, 1, 0), 3)
-  brier_score(probf, real)
-}
+source('./utils.R')
+
+test_that("getCoherentFlag", {
+  dts <- prepare_test_data1()
+  flags <- getCoherentFlag(dts$domain$incoherent_domain, dts$domain$coherent_domain)
+  expect_equal(flags$incoherent, c(2,3,4,6, 7, 9,10,11))
+  expect_equal(flags$coherent, c(1, 5, 8, 12))
+})
+
+test_that("brier Score", {
+  dts <- prepare_test_data1()
+  jdist_rec <- prepare_recdist(dts)
+  brier_score(jdist_rec, dts)
+  
+  basef <- marginal2Joint(prepare_basef(dts), dts$domain$incoherent_domain)
+  brier_score(basef, dts)
+})
+
+test_that("point forecasts", {
+  dts <- prepare_test_data1()
+  jdist_rec <- prepare_recdist(dts)
+  
+  pointf <- point_forecast(jdist_rec, dts$domain$coherent_domain)
+  expect_equal(pointf[,1], pointf[,2] + pointf[,3])
+  
+  mae <- function(x, y){mean(abs(x-y))}
+  rmse <- function(x, y){mean((x-y)^2)}
+  
+  point_metric(jdist_rec, dts, mae)
+  point_metric(jdist_rec, dts, rmse)
+})
